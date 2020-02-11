91ee10122dfc0a1f153fd9f05f8c53ed1bc9728b
Ivan Platonov <st049378@student.spbu.ru>
2020-02-11 12:15:07 +0300

    hello world

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	type Test struct {
+		Hello   string `json:"hello"`
+		TestVal int8   `json:"test_val"`
+	}
 	switch r.Method {
 	case "GET":
-		w.WriteHeader(http.StatusOK)
+		test := Test{Hello: "world", TestVal: 12}
+		testJson, _ := json.Marshal(&test)
+		w.Header().Set("Content-Type", "application/json")
+		if _, err := w.Write(testJson); err != nil {
+			panic(err)
+		}
+	case "POST":
+		var test Test
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := json.Unmarshal(data, &test); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		testJson, _ := json.Marshal(&test)
+		w.Header().Set("Content-Type", "application/json")
+		if _, err := w.Write(testJson); err != nil {
+			panic(err)
+		}
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
@@ -22,5 +48,5 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 	http.HandleFunc("/", indexHandler)
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
